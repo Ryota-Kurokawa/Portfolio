44f25458252bef42f591212cd8885804d9c919ba
ryota <kurokawa.ryota.1124@gmail.com>
2024-02-29 02:12:37 +0900

    修正

--- lib/presentation/page/my_skill_page.dart
+++ lib/presentation/page/my_skill_page.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:portfolio/presentation/comopnent/drower.dart';
 
 class MySkillPage extends StatelessWidget {
@@ -8,64 +9,90 @@ class MySkillPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: const ManageDrawer(),
-      body: Center(
-        child: Column(
-          children: [
-            CarouselSlider(
-              items: [
-                Card(child: Image.asset('images/C.png')),
-                Card(child: Image.asset('images/css.png')),
-                Card(child: Image.asset('images/Flutter.png')),
-                Card(child: Image.asset('images/Go.png')),
-                Card(child: Image.asset('images/html.png')),
-                Card(child: Image.asset('images/Rails.png')),
-                Card(child: Image.asset('images/SwiftUI.jpg')),
-              ],
-              options: CarouselOptions(
-                height: 300,
-                initialPage: 0,
-                autoPlay: true,
-                viewportFraction: 0.3,
-                enableInfiniteScroll: true,
-                autoPlayInterval: const Duration(seconds: 2),
-                autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                enlargeCenterPage: true,
-              ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'images/back1.jpg',
             ),
-            const SizedBox(height: 20),
-            const Text(
-              "The skill of the language & flamework I can use",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      context.pop('/top');
+                    },
+                    child: const Text(
+                      '←Return to Top',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 20),
-            CarouselSlider(
-              items: [
-                Card(child: Image.asset('images/C.png')),
-                Card(child: Image.asset('images/css.png')),
-                Card(child: Image.asset('images/Flutter.png')),
-                Card(child: Image.asset('images/Go.png')),
-                Card(child: Image.asset('images/html.png')),
-                Card(child: Image.asset('images/Rails.png')),
-                Card(child: Image.asset('images/SwiftUI.jpg')),
-              ],
-              options: CarouselOptions(
-                height: 300, //高さ
-                initialPage: 4, //最初に表示されるページ
-                autoPlay: true, //自動でスライドしてくれるか
-                viewportFraction: 0.3, //各カードの表示される範囲の割合
-                enableInfiniteScroll: true, //最後のカードから最初のカードへの遷移
-                autoPlayInterval: Duration(seconds: 2), //カードのインターバル
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                enlargeCenterPage: true, //選択したカードを中央に表示
-                //スライドが始まって終わるまでの時間
+              const SizedBox(
+                height: 100,
               ),
-            ),
-          ],
+              CarouselSlider(
+                items: [
+                  Image.asset('images/clang.png'),
+                  Image.asset('images/file_type_css_icon_130661.png'),
+                  Image.asset('images/Flutter.png'),
+                  Image.asset('images/golang_official_logo_icon_169092.png'),
+                  Image.asset('images/html.png'),
+                  Image.asset('images/Rails.png'),
+                  Image.asset('images/swiftui-128x128_2x.png'),
+                  Image.asset(
+                      'images/supabase-logo-DCC676FFE2-seeklogo.com.png'),
+                  Image.asset('images/nextjs-icon-512x309-yynfidez.png'),
+                  Image.asset('images/logo-logomark.png'),
+                ],
+                options: CarouselOptions(
+                  height: 300,
+                  initialPage: 0,
+                  autoPlay: true,
+                  viewportFraction: 0.3,
+                  enableInfiniteScroll: true,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                  enlargeCenterPage: true,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const SizedBox(height: 20),
+              CarouselSlider(
+                items: [
+                  Image.asset('images/clang.png'),
+                  Image.asset('images/file_type_css_icon_130661.png'),
+                  Image.asset('images/Flutter.png'),
+                  Image.asset('images/golang_official_logo_icon_169092.png'),
+                  Image.asset('images/html.png'),
+                  Image.asset('images/Rails.png'),
+                  Image.asset('images/swiftui-128x128_2x.png'),
+                  Image.asset(
+                      'images/supabase-logo-DCC676FFE2-seeklogo.com.png'),
+                  Image.asset('images/nextjs-icon-512x309-yynfidez.png'),
+                  Image.asset('images/logo-logomark.png'),
+                ],
+                options: CarouselOptions(
+                  height: 300, //高さ
+                  initialPage: 4, //最初に表示されるページ
+                  autoPlay: true, //自動でスライドしてくれるか
+                  viewportFraction: 0.3, //各カードの表示される範囲の割合
+                  enableInfiniteScroll: true, //最後のカードから最初のカードへの遷移
+                  autoPlayInterval: Duration(seconds: 2), //カードのインターバル
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  enlargeCenterPage: true, //選択したカードを中央に表示
+                  //スライドが始まって終わるまでの時間
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
